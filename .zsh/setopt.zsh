c56cb392a4eef843736bc7b82a063afa1dab751e
Mark Nichols <mark@zanshin.net>
2013-02-01 22:49:35 -0600

    experimenting with transient rprompt

--- .zsh/setopt.zsh
+++ .zsh/setopt.zsh
@@ -36,7 +36,7 @@ setopt correctall # spelling correction for arguments
 
 # ===== Prompt
 setopt prompt_subst # Enable parameter expansion, command substitution, and arithmetic expansion in the prompt
-setopt transient_rprompt #remove right prompt when accepting a command line
+setopt transient_rprompt # only show the rprompt on the current prompt
 
 # ===== Scripts and Functions
 setopt multios # perform implicit tees or cats when multiple redirections are attempted
