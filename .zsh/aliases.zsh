d5d2c4f32297ce2d1ac27707981cd4e0317e1ab8
Mark Nichols <mark@zanshin.net>
2013-02-06 15:16:49 -0600

    using eval stmt to set colors, and OS-specific alias for directory list cmds

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -24,11 +24,20 @@ alias 'bk=cd $OLDPWD'
 # -------------------------------------------------------------------
 # directory information
 # -------------------------------------------------------------------
-alias lh='ls -d .*' # show hidden files/directories only
-alias lsd='ls -aFhlG'
-alias l='ls -al'
-alias ls='ls -GFh' # Colorize output, add file type indicator, and put sizes in human readable format
-alias ll='ls -GFhl' # Same as above, but in long listing format
+if [[ $IS_MAC -eq 1 ]]; then
+    alias lh='ls -d .*' # show hidden files/directories only
+    alias lsd='ls -aFhlG'
+    alias l='ls -al'
+    alias ls='ls -GFh' # Colorize output, add file type indicator, and put sizes in human readable format
+    alias ll='ls -GFhl' # Same as above, but in long listing format
+fi
+if [[ $IS_LINUX -eq 1 ]]; then
+    alias lh='ls -d .* --color' # show hidden files/directories only
+    alias lsd='ls -aFhlG --color'
+    alias l='ls -al --color'
+    alias ls='ls -GFh --color' # Colorize output, add file type indicator, and put sizes in human readable format
+    alias ll='ls -GFhl --color' # Same as above, but in long listing format
+fi
 alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'"
 alias 'dus=du -sckx * | sort -nr' #directories sorted by size
 
