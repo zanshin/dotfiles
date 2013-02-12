a2fddd5a2682e401e89c074bd63ea484d551ab69
Mark Nichols <mark@zanshin.net>
2013-02-12 15:43:14 -0600

    added rmount and rumount functions to work wtih sshfs (fuse)

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -70,6 +70,42 @@ if [[ $IS_MAC -eq 1 ]]; then
 
     # notify function - http://hints.macworld.com/article.php?story=20120831112030251
     notify() { automator -D title=$1 -D subtitle=$2 -D message=$3 ~/Library/Workflows/DisplayNotification.wflow }
+
+    # Remote Mount (sshfs)
+    # creates mount folder and mounts the remote filesystem
+    rmount() {
+      local host folder mname
+      host="${1%%:*}:"
+      [[ ${1%:} == ${host%%:*} ]] && folder='' || folder=${1##*:}
+      if [[ -n $2 ]]; then
+        mname=$2
+      else
+        mname=${folder##*/}
+        [[ "$mname" == "" ]] && mname=${host%%:*}
+      fi
+      if [[ $(grep -i "host ${host%%:*}" ~/.ssh/config) != '' ]]; then
+        mkdir -p ~/mounts/$mname > /dev/null
+        sshfs $host$folder ~/mounts/$mname -oauto_cache,reconnect,defer_permissions,negative_vncache,volname=$mname,noappledouble && echo "mounted ~/mounts/$mname"
+      else
+        echo "No entry found for ${host%%:*}"
+        return 1
+      fi
+    }
+
+    # Remote Umount, unmounts and deletes local folder (experimental, watch you step)
+    rumount() {
+      if [[ $1 == "-a" ]]; then
+        ls -1 ~/mounts/|while read dir
+        do
+          [[ -d $(mount|grep "mounts/$dir") ]] && umount ~/mounts/$dir
+          [[ -d $(ls ~/mounts/$dir) ]] || rm -rf ~/mounts/$dir
+        done
+      else
+        [[ -d $(mount|grep "mounts/$1") ]] && umount ~/mounts/$1
+        [[ -d $(ls ~/mounts/$1) ]] || rm -rf ~/mounts/$1
+      fi
+    }
+
 fi
 
 # -------------------------------------------------------------------
