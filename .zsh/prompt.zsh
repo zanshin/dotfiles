d1a0362a4a4665a5957c898b4b7de725a546c017
Mark Nichols <mark@zanshin.net>
2013-03-21 16:00:36 -0500

    changed ruby prompt to use rvm current

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -86,7 +86,8 @@ function git_prompt_string() {
 function _update_ruby_version() {
     typeset -g ruby_version=''
     if which rvm-prompt &> /dev/null; then
-      ruby_version="$(rvm-prompt i v g)"
+      # ruby_version="$(rvm-prompt i v g)"
+      ruby_version="$(rvm current)"
     else
       if which rbenv &> /dev/null; then
         ruby_version="$(rbenv version | sed -e "s/ (set.*$//")"
