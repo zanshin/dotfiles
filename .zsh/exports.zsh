1c7185b8834e1ce4497dc5b310f6119b1ffc82af
Mark Nichols <mark@zanshin.net>
2013-03-29 14:58:52 -0500

    removed duplicate entry from $PATH

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,10 +1,11 @@
 # Currently this path is appended to dynamically when picking a ruby version
 # zshenv has already started PATH with rbenv so append only here
-export PATH=$PATH:/usr/local/bin:/usr/local/sbin:$HOME/bin
+#export PATH=$PATH:/usr/local/bin:/usr/local/sbin:$HOME/bin
+export PATH=$PATH:/usr/local/sbin:$HOME/bin
 
 # Set default console Java to 1.6
 if [[ $IS_MAC -eq 1 ]]; then
-    export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+    export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
 fi
 
 # Setup terminal, and turn on colors
@@ -31,7 +32,7 @@ fi
 #export NODE_PATH=/opt/github/homebrew/lib/node_modules
 #export PYTHONPATH=/usr/local/lib/python2.6/site-packages
 # CTAGS Sorting in VIM/Emacs is better behaved with this in place
-export LC_COLLATE=C 
+export LC_COLLATE=C
 
 #export GH_ISSUE_CREATE_TOKEN=083f60c674d8eb41f98258df9fc8d94cb733218a
 
@@ -39,5 +40,5 @@ export LC_COLLATE=C
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/Projects/django
 if [[ $HAS_VIRTUALENV -eq 1 ]]; then
-    source /usr/local/bin/virtualenvwrapper.sh 
+    source /usr/local/bin/virtualenvwrapper.sh
 fi
