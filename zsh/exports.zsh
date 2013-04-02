316a2be3f61c927d7fd20b59729094d2d62cf4a9
Mark Nichols <mark@zanshin.net>
2013-04-01 22:51:40 -0500

    Added LC_ALL settings for UTF8.
    tmux.conf already had UTF specified, but zshrc didn't. Adding and export of
    LC_ALL="en_US.UTF-8" and LC_CTYPE="en_US.UTF8" cleared up an extra space that was
    shoving the right-prompt off the right edge of the screen.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -32,12 +32,12 @@ if [[ $IS_LINUX -eq 1 ]]; then
     export EDITOR='vim'
 fi
 
-#export NODE_PATH=/opt/github/homebrew/lib/node_modules
-#export PYTHONPATH=/usr/local/lib/python2.6/site-packages
 # CTAGS Sorting in VIM/Emacs is better behaved with this in place
 export LC_COLLATE=C
 
-#export GH_ISSUE_CREATE_TOKEN=083f60c674d8eb41f98258df9fc8d94cb733218a
+# Set LC_ALL="UTF8"
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
 
 # Virtual Environment Stuff
 export WORKON_HOME=$HOME/.virtualenvs
