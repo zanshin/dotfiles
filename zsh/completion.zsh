9b779a9a6bccb68118b828e5d6d76fc78d1b42fe
Mark Nichols <mark@zanshin.net>
2014-05-23 13:06:13 -0500

    Added completion.local directory
    Added _kitchen completion code from https://gist.github.com/petere/10307599
    Updated completion.zsh to add path to completion.local directory to $FPATH

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,8 @@
-autoload -U compinit && compinit                                                                                       
+autoload -U compinit && compinit
 zmodload -i zsh/complist
+
+# Add path to my completions
+fpath=(~/.zsh/completion.local $fpath)
 
 # man zshcontrib
 zstyle ':vcs_info:*' actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
