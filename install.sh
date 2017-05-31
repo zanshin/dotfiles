e21fb9673869b8f2a6bf48568cd80e7b08593da0
Mark Nichols <mark@zanshin.net>
2017-05-30 23:36:56 -0500

    Update ask function
    Updated ask function and added attribution

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-
 set -e
+
 ask() {
+  # https://djm.me/ask
+  local prompt default reply
+
   while true; do
+
     if [ "${2:-}" = "Y" ]; then
       prompt="Y/n"
       default=Y
@@ -13,14 +17,24 @@ ask() {
       prompt="y/n"
       default=
     fi
-    read -p "$1 [$prompt] " REPLY </dev/tty
-    if [ -z "$REPLY" ]; then
-      REPLY=$default
+
+    # Ask the question (not using "read -p" as it uses stderr not stdout)
+    echo -n "$1 [$prompt] "
+
+    # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+    read reply </dev/tty
+
+    # Default?
+    if [ -z "$reply" ]; then
+      reply=$default
     fi
-    case "$REPLY" in
+
+    # Check if the reply is valid
+    case "$reply" in
       Y*|y*) return 0 ;;
       N*|n*) return 1 ;;
     esac
+
   done
 }
 
