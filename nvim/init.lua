88c1f21b3f9f39483939237f3cc2ec01089e85ee
Mark Nichols <mark@zanshin.net>
2022-03-03 15:16:51 -0600

    Name spaced my lua files under usr

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,10 @@
 -- Neovim Configuration
 -- February 2022
 
-require 'helpers'        -- Functions to wrap commands
-require 'options'        -- Anything that gets set
-require 'plugins'        -- All the plugins
-require 'mappings'       -- All the mappings
-require 'autocmds'       -- Auto commands
-require 'colors'         -- Theme
+require 'usr.helpers'        -- Functions to wrap commands
+require 'usr.options'        -- Anything that gets set
+require 'usr.plugins'        -- All the plugins
+require 'usr.mappings'       -- All the mappings
+require 'usr.autocmds'       -- Auto commands
+require 'usr.colors'         -- Theme
 
