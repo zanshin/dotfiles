e3fe2cf782b04acae70641000e2cea1d944e4539
Mark Nichols <mark@zanshin.net>
2022-03-04 17:44:32 -0600

    simplifed by removing icons

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -23,87 +23,6 @@ end
 
 -- set completeopt=menu,menuone,noselect
 
--- setup lspkind
-local lspkind_status_ok, lspkind = pcall(require, "lspkind")
-if not lspkind_status_ok then
-  vim.notify("lspkind not available.")
-end
-lspkind.init({
-
-    -- defines how annotations are shown
-    -- default: symbol
-    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
-    mode = 'symbol_text',
-
-    -- default symbol map
-    -- can be either 'default' (requires nerd-fonts font) or
-    -- 'codicons' for codicon preset (requires vscode-codicons font)
-    --
-    -- default: 'default'
-    preset = 'codicons',
---
---     -- override preset symbols
---     --
---     -- default: {}
---     symbol_map = {
---       Text = "",
---       Method = "",
---       Function = "",
---       Constructor = "",
---       Field = "ﰠ",
---       Variable = "",
---       Class = "ﴯ",
---       Interface = "",
---       Module = "",
---       Property = "ﰠ",
---       Unit = "塞",
---       Value = "",
---       Enum = "",
---       Keyword = "",
---       Snippet = "",
---       Color = "",
---       File = "",
---       Reference = "",
---       Folder = "",
---       EnumMember = "",
---       Constant = "",
---       Struct = "פּ",
---       Event = "",
---       Operator = "",
---       TypeParameter = ""
---     },
-})
-
--- Icons for various kinds of things
--- local kind_icons = {
---   Text = "",
---   Method = "m",
---   Function = "",
---   Constructor = "",
---   Field = "",
---   Variable = "",
---   Class = "",
---   Interface = "",
---   Module = "",
---   Property = "",
---   Unit = "",
---   Value = "",
---   Enum = "",
---   Keyword = "",
---   Snippet = "",
---   Color = "",
---   File = "",
---   Reference = "",
---   Folder = "",
---   EnumMember = "",
---   Constant = "",
---   Struct = "",
---   Event = "",
---   Operator = "",
---   TypeParameter = "",
--- }
--- find more here: https://www.nerdfonts.com/cheat-sheet
-
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -166,25 +85,16 @@ cmp.setup {
   },
 
   formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol', -- show only symbol annotations
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+    fields = { "menu", "abbr" },
 
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      -- before = function (entry, vim_item)
-      --   ...
-      --   return vim_item
-      -- end
-    }),
-
-    menu = {
-      buffer = "[BUFFER]",
-      nvm_lsp = "[LSP]",
-      nvim_lua = "[API]",
-      luasnip = "[LUASNIP]",
-      path = "[PATH]",
-    },
+    format = function(entry, vim_item)
+      vim_item.menu = ({
+        luasnip = "[Snippet]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      })[entry.source.name]
+      return vim_item
+    end,
   },
 
   documentation = {
