42c4bc793acef04575723eeacac259ecc92a6ac1
Mark Nichols <mark@zanshin.net>
2022-03-04 16:12:58 -0600

    formatting

--- nvim/lua/usr/plugins.lua
+++ nvim/lua/usr/plugins.lua
@@ -158,8 +158,8 @@ return packer.startup(function(use)
   use { 'hrsh7th/cmp-nvim-lsp' }   -- lsp completions
 
   -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  use { 'L3MON4D3/LuaSnip' }       --snippet engine
+  use { 'rafamadriz/friendly-snippets' } -- a bunch of snippets to use
 
   -- VS Code Icons
   use { 'mortepau/codicons.nvim' }
