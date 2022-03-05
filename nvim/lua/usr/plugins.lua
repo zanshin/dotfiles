f2cabd2c4739a8a490c2c226c50d838a261e9c2d
Mark Nichols <mark@zanshin.net>
2022-03-05 15:32:25 -0600

    adding treesitter and its config

--- nvim/lua/usr/plugins.lua
+++ nvim/lua/usr/plugins.lua
@@ -134,9 +134,12 @@ return packer.startup(function(use)
     config = get_config("telescope")
   }
 
-  -- -- Treesitter
-  -- use { 'nvim-treesitter/nvim-treesitter' }
-  -- use { 'nvim-treesitter/playground' }
+  -- Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use { 'nvim-treesitter/playground' }
 
   -- Completion
   use {
