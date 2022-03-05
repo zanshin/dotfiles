2a587f606bdbe0431fb152f66aeb75a9aae936cc
Mark Nichols <mark@zanshin.net>
2022-03-05 15:59:30 -0600

    added protected require call

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -1,5 +1,10 @@
 -- lualine
-require'lualine'.setup{
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+  return
+end
+
+lualine.setup {
   options = {
     icons_enabled = false,
     theme = 'material',
