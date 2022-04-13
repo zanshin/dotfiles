3b26ca83584151f131b12768387903810e967d63
Mark Nichols <mark@zanshin.net>
2022-04-13 17:16:27 -0500

    Added function to wrap goimports
    Function is called from autocmd when in a Go file

--- nvim/lua/usr/autocmds.lua
+++ nvim/lua/usr/autocmds.lua
@@ -122,10 +122,10 @@ acmd({ "BufWritePre" },
        command = "lua vim.lsp.buf.formatting_sync()",
        group = _go})
 
--- acmd({ "BufWritePre" },
---      { pattern = "*.go",
---        command = "!goimports:",
---        group = _go})
+acmd({ "BufWritePre" },
+     { pattern = "*.go",
+       command = "lua OrgImports(1000)",
+       group = _go})
 
 -- Text files
 local _text = agrp("_text", { clear = true  })
@@ -160,3 +160,19 @@ acmd({ "FileWritePre", "FileAppendPre", "FilterWritePre", "BufWritePre" },
     endfunction
   ]], false)
 
+-- Organize Go imports
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
+function OrgImports(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = {only = {"source.organizeImports"}}
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
