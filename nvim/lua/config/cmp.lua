21ced6f73d6360c6c58ec0ba2e56513351b40309
Mark Nichols <mark@zanshin.net>
2022-05-04 22:13:26 -0500

    Flip completion to explicit
    <CR> now completes items explicitly selected

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -50,7 +50,7 @@ cmp.setup {
     },
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<CR>"] = cmp.mapping.confirm { select = false},
 
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
