b3f346dd3281aabfee33f3b66c01064e498540e1
Mark Nichols <mark@zanshin.net>
2022-05-10 13:26:04 -0500

    Address issues with nvim-tree changes
    https://github.com/LunarVim/Neovim-from-scratch/issues/124

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -21,6 +21,15 @@ vim.g.nvim_tree_icons = {
   },
 }
 
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_show_icons = {
+  git = 1,
+  folders = 1,
+  files = 1,
+  folder_arrows = 1,
+}
+vim.g.nvim_tree_root_folder_modifier = ':t'
+
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
@@ -86,15 +95,23 @@ nvim_tree.setup {
     number = false,
     relativenumber = false,
   },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
+  actions = {
+    open_file = {
+      quit_on_open = false,
+      window_picker = {
+        enable = true,
+      },
+    },
   },
+  -- quit_on_open = 0,
+  -- git_hl = 1,
+  -- disable_window_picker = 0,
+  -- root_folder_modifier = ":t",
+  -- show_icons = {
+  --   git = 1,
+  --   folders = 1,
+  --   files = 1,
+  --   folder_arrows = 1,
+  --   tree_width = 30,
+  -- },
 }
