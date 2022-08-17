4ad0344e25c5f0320f21359a91ff4114b3cbabf5
Mark Nichols <mark@zanshin.net>
2022-08-17 14:45:14 -0500

    Changed variable name to eliminate dashes

--- nvim/lua/config/nvim-dap-virtual-text.lua
+++ nvim/lua/config/nvim-dap-virtual-text.lua
@@ -1,6 +1,6 @@
-local status_ok, nvim-dap-virtual-text = pcall(require, "nvim-dap-virtual-text")
+local status_ok, virtual_text = pcall(require, "nvim-dap-virtual-text")
 if not status_ok then
   return
 end
 
-nvim-dap-virtual-text.setup()
+virtual_text.setup()
