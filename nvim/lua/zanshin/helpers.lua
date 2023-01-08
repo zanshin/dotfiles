6650945f913578bac9e8bff555e528ecd0ff727c
Mark Nichols <mark@zanshin.net>
2023-01-08 16:41:27 -0600

    formatting fixes

--- nvim/lua/zanshin/helpers.lua
+++ nvim/lua/zanshin/helpers.lua
@@ -1,15 +1,15 @@
 -- Helpers
-cmd = vim.cmd    -- to execute Vim commands. E.g., cmd('pwd')
-fn = vim.fn      -- to call Vim functions. E.g., fn.bunnr()
-g = vim.g        -- a table to access global variables
-opt = vim.opt    -- to set options
-api = vim.api    -- for api calls
+cmd = vim.cmd -- to execute Vim commands. E.g., cmd('pwd')
+fn = vim.fn -- to call Vim functions. E.g., fn.bunnr()
+g = vim.g -- a table to access global variables
+opt = vim.opt -- to set options
+api = vim.api -- for api calls
 
-execute = vim.api.nvim_command
+-- Execute = vim.api.nvim_command
 
 -- Create mappings with noremap option set to true
 function map(mode, lhs, rhs, opts)
- local options = {noremap = true, silent = true}
- if opts then options = vim.tbl_extend('force', options, opts) end
- vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  local options = { noremap = true, silent = true }
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
