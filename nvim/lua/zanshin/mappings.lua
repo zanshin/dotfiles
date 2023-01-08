34d1097e01f6e1c8b2b42a130b2d8a221e297f4a
Mark Nichols <mark@zanshin.net>
2023-01-08 16:41:56 -0600

    changed mapping for fuzzy search in buffer

--- nvim/lua/zanshin/mappings.lua
+++ nvim/lua/zanshin/mappings.lua
@@ -92,13 +92,13 @@ map('n', '<leader>tsh', ':TSHighlightCapturesUnderCursor<CR>')
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
+vim.keymap.set('n', '<leader>+', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
     previewer = false,
   })
-end, { desc = '[/] Fuzzily search in current buffer]' })
+end, { desc = '[+] Fuzzily search in current buffer]' })
 
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
