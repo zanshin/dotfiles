c275d4f4f2a44f1e2fff0adddc8245414b866f21
Mark Nichols <mark@zanshin.net>
2023-02-04 08:00:03 -0600

    Removed deprecated options for nvim-tree

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -44,12 +44,6 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   -- auto_close = true,
   open_on_tab = false,
   hijack_cursor = false,
