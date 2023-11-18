e92e37d0ce7a0dd205bb733f99cc8acd08ed9c44
Mark Nichols <mark@zanshin.net>
2023-11-18 12:50:56 -0600

    Switched to horizontal display for undo previewer

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -10,7 +10,7 @@ telescope.setup {
     undo = {
       -- telescope-undo configurations
       side_by_side = true,
-      layout_strategy = "vertical",
+      layout_strategy = "horizontal",
       layout_config = {
         preview_height = 0.8,
       },
