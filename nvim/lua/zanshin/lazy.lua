8ab641a300963bd262c06f28e39ad8a8b169bc4f
Mark Nichols <mark@zanshin.net>
2023-11-19 12:46:41 -0600

    Turned off change notifications from lazy.nvim

--- nvim/lua/zanshin/lazy.lua
+++ nvim/lua/zanshin/lazy.lua
@@ -11,4 +11,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("zanshin.plugins", opts)
+require("lazy").setup("zanshin.plugins", { change_detection = { nofity = false, }, })
