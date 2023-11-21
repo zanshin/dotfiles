52b664dd31c5293e1de26c3927d706755773d657
Mark Nichols <mark@zanshin.net>
2023-11-20 22:43:36 -0600

    Cleaned up neo-tree configuration

--- nvim/after/plugin/neo-tree.lua
+++ nvim/after/plugin/neo-tree.lua
@@ -159,7 +159,9 @@ neotree.setup({
         --".null-ls_*",
       },
     },
-    follow_current_file = false,            -- This will find and focus the file in the active buffer every
+    follow_current_file = {
+      enabled = false
+    },                                      -- This will find and focus the file in the active buffer every
     -- time the current file is changed while the tree is open.
     group_empty_dirs = false,               -- when true, empty folders will be grouped together
     hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
@@ -194,9 +196,11 @@ neotree.setup({
     commands = {} -- Add a custom command or override a global one using the same function name
   },
   buffers = {
-    follow_current_file = true, -- This will find and focus the file in the active buffer every
+    follow_current_file = {
+      enabled = true
+    },                       -- This will find and focus the file in the active buffer every
     -- time the current file is changed while the tree is open.
-    group_empty_dirs = true,    -- when true, empty folders will be grouped together
+    group_empty_dirs = true, -- when true, empty folders will be grouped together
     show_unloaded = true,
     window = {
       mappings = {
