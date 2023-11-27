870027465d12d77617fe8fca7b8804fbae83aa86
Mark Nichols <mark@zanshin.net>
2023-11-26 20:33:15 -0600

    Switch to alternate web devicons source

--- nvim/lua/zanshin/plugins.lua
+++ nvim/lua/zanshin/plugins.lua
@@ -5,7 +5,7 @@ return {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- Snazzy glyphs
-  'kyazdani42/nvim-web-devicons',
+  -- 'kyazdani42/nvim-web-devicons',
 
   -- Lualine
   'nvim-lualine/lualine.nvim',
@@ -55,8 +55,7 @@ return {
       'nvim-lua/popup.nvim',
       'nvim-lua/plenary.nvim',
       'debugloop/telescope-undo.nvim',
-      'kyazdani42/nvim-web-devicons',
-      -- 'nvim-tree/nvim-web-devicons',
+      'nvim-tree/nvim-web-devicons',
     },
   },
 
@@ -104,8 +103,7 @@ return {
     branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      'kyazdani42/nvim-web-devicons',
-      -- "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
