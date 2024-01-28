f72f923e285f00e561dac5fc818e7c1d4cb20ec5
Mark Nichols <mark@zanshin.net>
2024-01-27 21:52:47 -0600

    File explorer stupidness

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -3,11 +3,22 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons"
   },
-  opts = {
-    indicator = {
-      icon = '>> ',
-      style = 'icon',
+  options = {
+    offsets = {
+      {
+        filetype = "neo-tree",
+        text = "Finder",
+        -- text = function()
+        --   return vim.fn.getcwd()
+        -- end,
+        highlight = "Directory",
+        separator = true
+      },
     },
+    -- indicator = {
+    --   icon = '| ',
+    --   style = 'icon',
+    -- },
   },
   config = function()
     require("bufferline").setup {}
