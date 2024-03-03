00471160946c1bd4f9d3ffd0a1d6ddef34be760d
Mark Nichols <mark@zanshin.net>
2024-03-03 14:00:02 -0600

    Mappings now use vim.keymap.set and have descriptions

--- nvim/lua/zanshin/mappings.lua
+++ nvim/lua/zanshin/mappings.lua
@@ -1,13 +1,4 @@
 -- Mappings
--- Create mappings with noremap option set to true
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
-local g = vim.g -- a table to access global variables
-
 -- Modes
 --   normal_mode = "n"
 --   insert_mode = "i"
@@ -16,27 +7,33 @@ local g = vim.g -- a table to access global variables
 --   term_mode = "t"
 --   command_mode = "c"
 
-map("", "<Space>", "<Nop>") -- map Space to a no-op
-g.mapleader = ' '           -- space for leader
-g.maplocalleader = ' '      -- space for local leader too
-map('n', ';', ':')          -- use ; for : in normal mode
-map('n', ';;', ';')         -- use ;; for ; in normal mode
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message.' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- map("", "<Space>", "<Nop>") -- map Space to a no-op
+-- g.mapleader = ' '           -- space for leader
+-- g.maplocalleader = ' '      -- space for local leader too
+vim.keymap.set('n', ';', ':', { desc = 'Map ; to be :' })
+vim.keymap.set('n', ';;', ';', { desc = 'Map ;; to be ;' })
 
 -- Disable arrow keys and make semi-snarky comment instead
-map('n', '<Left>', ':echo "Use h"<CR>')
-map('n', '<Right>', ':echo "Use l"<CR>')
-map('n', '<Up>', ':echo "Use k"<CR>')
-map('n', '<Down>', ':echo "Use j"<CR>')
+vim.keymap.set('n', '<Left>', '<cmd>echo "Use h"<CR>')
+vim.keymap.set('n', '<Right>', '<cmd>:echo "Use l"<CR>')
+vim.keymap.set('n', '<Up>', '<cmd>echo "Use k"<CR>')
+vim.keymap.set('n', '<Down>', '<cmd>echo "Use j"<CR>')
 
 -- Use Control-hjkl to switch between splits
-map('n', '<S-h>', '<C-w>h')
-map('n', '<S-j>', '<C-w>j')
-map('n', '<S-k>', '<C-w>k')
-map('n', '<S-l>', '<C-w>l')
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the upper window' })
 
 -- Move to first non-blank or last non-blank character in current line
-map('n', 'F', '^')
-map('n', 'L', 'g_')
+vim.keymap.set('n', 'F', '^', { desc = 'Move to first non-blank character in the line' })
+vim.keymap.set('n', 'L', 'g_', { desc = 'Move to last non-blank character in the line' })
 
 -- Fix regex handling to be "normal"
 -- map('n', '/', '/\v')
@@ -44,17 +41,18 @@ map('n', 'L', 'g_')
 
 -- Map escape to jj -- faster to type
 --Also, jk and kj. The more the merrier
-map('i', 'jj', '<ESC>')
-map('i', 'jk', '<ESC>')
-map('i', 'kj', '<ESC>')
+vim.keymap.set('i', 'jj', '<ESC>', { desc = 'Use jj as an ESC key substitute' })
+vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Use jk as an ESC key substitute' })
+vim.keymap.set('i', 'kj', '<ESC>', { desc = 'Use kj as an ESC key substitute' })
 
 -- Remove search highlighting with <leader>/
-map('n', '<leader>/', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>/', ':nohlsearch<CR>', { desc = 'Remove search highlighting' })
 
 -- Relative number / absolue numbr toggle
-map('n', '<F10>', ':set relativenumber! number! number?<CR>')
-map('n', 'nn', ':set norelativenumber<cr>')
-map('n', 'rr', ':set relativenumber<cr>')
+vim.keymap.set('n', '<F10>', ':set relativenumber! number! number?<CR>',
+  { desc = 'Toggle between number and relative number' })
+vim.keymap.set('n', 'nn', ':set norelativenumber<cr>', { desc = 'Turn relative line numbering off' })
+vim.keymap.set('n', 'rr', ':set relativenumber<cr>', { desc = 'Turn relative line numbering on' })
 
 -- Move line(s) up or down via J and K respectively
 -- map('v', '<C-j>', ":m '>+1<CR>gv=gv")
@@ -72,86 +70,86 @@ map('n', 'rr', ':set relativenumber<cr>')
 -- map('v', '<C-u>', ':m >-2<cr>gv=gv')
 
 -- Scroll 1/2 a page and center
-map("n", "<C-d>", "<C-d>zz")
-map("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Scroll [D]own half a page and then center that line' })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Scroll [U]p half a page and then center that line' })
 
 -- Override n/N to show next/previous search result, but centered
-map("n", "n", "nzzzv")
-map("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv", { desc = 'Move to [N] search result and center in the window' })
+vim.keymap.set("n", "N", "Nzzzv", { desc = 'Move to [N] search result and center in the window' })
 
 -- Buffers and tabs
-map('n', '<leader>be', ':enew<CR>')          -- create new empty buffer
-map('n', '<leader>bn', ':bnext<CR>')         -- move to next buffer
-map('n', '<leader>bp', ':bprevious<CR>')     -- move to previous buffer
-map('n', '<leader>bc', ':bp <BAR> bd $<CR>') -- move to previous buffer, and close current
+vim.keymap.set('n', '<leader>be', ':enew<CR>', { desc = 'Create a new empty buffer' })
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = 'Move to the next buffer' })
+vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { desc = 'Move to the previous buffer' })
+vim.keymap.set('n', '<leader>bc', ':bp <BAR> bd $<CR>', { desc = 'Move to the previous buffer, and close the current' })
 
-map('n', '<S-n>', ':bnext<CR>')              -- move to next buffer
-map('n', '<S-p>', ':bprevious<CR>')          -- move to previous buffer
+vim.keymap.set('n', '<S-n>', ':bnext<CR>', { desc = 'Move to the [N]ext buffer' })
+vim.keymap.set('n', '<S-p>', ':bprevious<CR>', { desc = 'Move to the [P]revious buffer' })
 
 -- Sudo to write protected file
-map('c', 'w!!', '!sudo tee % >/dev/null')
+vim.keymap.set('c', 'w!!', '!sudo tee % >/dev/null', { desc = 'Use sudo to write protected file' })
 
 -- Toggle listchars on / off
-map('n', '<leader>l', ':set list!<cr>')
+vim.keymap.set('n', '<leader>l', ':set list!<cr>', { desc = 'Toggle [L]istchars on or off' })
 
 -- Splits
-map('n', '<leader>v', '<C-w>v<C-w>l') -- open a vertical split and switch to it
-map('n', '<leader>h', '<C-w>s<C-w>j') -- open a horizontal split and switch to it
+vim.keymap.set('n', '<leader>v', '<C-w>v<C-w>l', { desc = 'Open a [V]ertical split and move focus there' })   -- open a vertical split and switch to it
+vim.keymap.set('n', '<leader>h', '<C-w>s<C-w>j', { desc = 'Open a [H]orizontal split and move focus there' }) -- open a horizontal split and switch to it
 
 -- System clipboard
-map('n', '<leader>y', '"+y')
-map('n', '<leader>p', '"+p')
-map('n', '<leader>d', '"+d')
+-- map('n', '<leader>y', '"+y')
+-- map('n', '<leader>p', '"+p')
+-- map('n', '<leader>d', '"+d')
 
 -- Use jq to format JSON
-map('n', '<leader>jq', ':%!jq .<cr>')
+vim.keymap.set('n', '<leader>jq', ':%!jq .<cr>', { desc = 'Format current buffer using JQ' })
 
 -- Neovim configuration file
-map('n', '<leader>ev', '<cmd>e $MYVIMRC<CR>') -- open configuration file for editing
--- map('n', '<leader>sv', ':luafile %<CR>')      -- source nvim configuration
-map('n', '<silent><leader>sv', ':source ~/.config/nvim/init.lua<CR>:filetype detect<CR>')
+vim.keymap.set('n', '<leader>ev', '<cmd>e $MYVIMRC<CR>', { desc = 'Open NVIM configuration file for editing' })
+vim.keymap.set('n', '<silent><leader>sv', ':source ~/.config/nvim/init.lua<CR>:filetype detect<CR>',
+  { desc = 'Source NVIM configuration file' })
 
--- Quickfix
-map('n', '<leader>qn', ':cnext<cr>') -- jump to next quickfix list item
-map('n', '<leader>qp', ':cprev<cr>') -- jump to prev quickfix list item
+-- Quickfix navigation
+vim.keymap.set('n', '<leader>qn', ':cnext<cr>', { desc = 'Move to the [N]ext Quickfix list item' })
+vim.keymap.set('n', '<leader>qp', ':cprev<cr>', { desc = 'Move to the [P]revious Quickfix list item' })
 
 -- -----------------------------------------------------------------------
 -- Plugin mappings
 -- -----------------------------------------------------------------------
 -- Treesitter mappings
-map('n', '<leader>tsp', ':TSPlaygroundToggle<CR>')
-map('n', '<leader>tsh', ':TSHighlightCapturesUnderCursor<CR>')
-map('n', '<leader>U', ':Telescope undo<cr>')
+vim.keymap.set('n', '<leader>tsp', ':TSPlaygroundToggle<CR>', { desc = '[T]oggle [T]ree[S]itter playground' })
+vim.keymap.set('n', '<leader>tsh', ':TSHighlightCapturesUnderCursor<CR>', { desc = '[T]ree[S]itter [H]ighlight' })
 
 -- Telescope mappings
-map('n', '<leader>ff', ':Telescope find_files<CR>')
-map('n', '<leader>fh', ':Telescope help_tags<CR>')
-map('n', '<leader>fw', ':Telescope live_grep<CR>')
-map('n', '<Leader>fb', ':Telescope buffers<CR>')
-map('n', '<Leader>fo', ':Telescope commands<CR>')
-map('n', '<Leader>fgs', ':Telescope git_status<CR>')
-map('n', '<Leader>fgf', ':Telescope git_files<CR>')
-map('n', '<Leader>fgc', ':Telescope git_commits<CR>')
-map('n', '<Leader>fgb', ':Telescope git_branches<CR>')
-map('n', '<Leader>fgt', ':Telescope git_stash<CR>')
+vim.keymap.set('n', '<leader>U', ':Telescope undo<cr>', { desc = '[U]ndo using Telescope' })
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', { desc = 'Telescope [F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<CR>', { desc = 'Telescop open help' })
+vim.keymap.set('n', '<leader>fw', ':Telescope live_grep<CR>', { desc = 'Telescop live grep' })
+vim.keymap.set('n', '<Leader>fb', ':Telescope buffers<CR>', { desc = 'Telescope list buffers' })
+vim.keymap.set('n', '<Leader>fo', ':Telescope commands<CR>', { desc = 'Telescope list commands' })
+vim.keymap.set('n', '<Leader>fgs', ':Telescope git_status<CR>', { desc = 'Telescope Git status' })
+vim.keymap.set('n', '<Leader>fgf', ':Telescope git_files<CR>', { desc = 'Telescope Git files' })
+vim.keymap.set('n', '<Leader>fgc', ':Telescope git_commits<CR>', { desc = 'Telescope Display Git Commit History' })
+vim.keymap.set('n', '<Leader>fgb', ':Telescope git_branches<CR>', { desc = 'Telescope Display Git branches' })
+-- vim.keymap.set('n', '<Leader>fgt', ':Telescope git_stash<CR>', { desc = 'Telescope Git Stash' })
 
 -- Neo-tree
-map('n', '\\', ':Neotree reveal<cr>')
-map('n', '<C-n>', ':Neotree toggle<cr>')
+vim.keymap.set('n', '\\', ':Neotree reveal<cr>', { desc = 'Reveal Neotree window' })
+vim.keymap.set('n', '<C-n>', ':Neotree toggle<cr>', { desc = 'Toggle [N]eotree window' })
 
 -- nvim-tree
 -- map('n', '<C-n>', ':NvimTreeToggle<cr>')
 -- map('n', '<leader>r', ':NvimTreeRefresh')
 
 -- DAP mappings -- moved to plugin configs
-map("n", "<leader>dt", ":lua require'dapui'.DapUiToggle<cr>")
-map("n", "<leader>dc", ":lua require'dap'.continue()<cr>")
-map("n", "<leader>dsi", ":lua require'dap'.step_into()<cr>")
-map("n", "<leader>dso", ":lua require'dap'.step_over()<cr>")
-map("n", "<leader>dsu", ":lua require'dap'.step_out()<cr>")
-map("n", "<leader>db", ":lua require'dap'.toggle_breakpoint()<cr>")
+vim.keymap.set("n", "<leader>dt", ":lua require'dapui'.DapUiToggle<cr>", { desc = 'DAP [T]oggle UI' })
+vim.keymap.set("n", "<leader>dc", ":lua require'dap'.continue()<cr>", { desc = 'DAP [C]ontinue' })
+vim.keymap.set("n", "<leader>dsi", ":lua require'dap'.step_into()<cr>", { desc = 'DAP [S]tep [I]nto' })
+vim.keymap.set("n", "<leader>dso", ":lua require'dap'.step_over()<cr>", { desc = 'DAP [S]tep [O]ver' })
+vim.keymap.set("n", "<leader>dsu", ":lua require'dap'.step_out()<cr>", { desc = 'DAP [S]tep o[U]t' })
+vim.keymap.set("n", "<leader>db", ":lua require'dap'.toggle_breakpoint()<cr>", { desc = 'DAP toggle [B]reakpoint' })
 -- map("n", "<leader>dr", ":lua require'dap'.repl.open()<cr>")
-map("n", "<leader>dr", ":lua require'dapui'.open({reset = true})<cr>")
+vim.keymap.set("n", "<leader>dr", ":lua require'dapui'.open({reset = true})<cr>", { desc = 'DAP [R]eset UI' })
 
 -- Git Blame
-map("n", "<leader>gb", ":GitBlameToggle<cr>")
+vim.keymap.set("n", "<leader>gb", ":GitBlameToggle<cr>", { desc = 'Toggle Git [B]lame on / off' })
