c2cb6632c8e132c8452b6b8c7c5610f027cf6da1
Mark Nichols <mark@zanshin.net>
2024-05-01 20:57:48 -0500

    Telescope added branch and dependencies

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,7 @@
 return {
   "nvim-telescope/telescope.nvim",
+  branch = '0.1.x',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local actions = require("telescope.actions")
 
