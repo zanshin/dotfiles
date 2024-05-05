afe36a09329196ac9c2c4ee8e78e075d96a64c52
Mark Nichols <mark@zanshin.net>
2024-05-05 08:27:16 -0500

    Go lang format autocmd moved here

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -1,5 +1,38 @@
 -- Go language specific settings
-vim.opt.set.tabstop = 4
-vim.opt.set.softtabstop = 4
-vim.opt.set.shiftwidth = 4
-vim.opt.setexpandtab = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+
+-- Organize Go imports
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
+-- https://github.com/neovim/nvim-lspconfig/issues/115#issuecomment-902680058
+function OrgImports(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
+
+local go_lsp = vim.api.nvim_create_augroup("go_lsp", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWritePre" },
+  {
+    pattern = "*.go",
+    command = ":silent lua vim.lsp.buf.format({ async = true })",
+    group = go_lsp
+  })
+vim.api.nvim_create_autocmd({ "BufWritePre" },
+  {
+    pattern = "*.go",
+    command = ":silent! lua OrgImports(3000)",
+    group = go_lsp
+  })
