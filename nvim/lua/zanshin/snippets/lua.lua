ef678c91a59d051c97cf1d9764b6673c04f2ecbc
Mark Nichols <mark@zanshin.net>
2024-05-25 00:00:58 -0500

    Lua file type snippet file

--- nvim/lua/zanshin/snippets/lua.lua
+++ nvim/lua/zanshin/snippets/lua.lua
@@ -3,27 +3,14 @@ require("luasnip.session.snippet_collection").clear_snippets "lua"
 local ls = require "luasnip"
 
 local s = ls.snippet
-local i = ls.insert_code
+local i = ls.insert_node
+local t = ls.text_node
 
 local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("lua", {
-  -- -- format snippet 
-  -- s("snipf", fmt([[ 
-  --     <>({ trig='<>', name='<>', dscr='<>'},
-  --     fmt(<>,
-  --     { <> },
-  --     { delimiters='<>' }
-  --     )<>)<>,]],
-  --     { c(1, {t("s"), t("autosnippet")}), i(2, "trig"), i(3, "trig"), i(4, "dscr"), i(5, "fmt"), i(6, "inputs"), i(7, "<>"), i(8, "opts"), i(0)},
-  --     { delimiters='<>' }
-  --     )),
+-- vim.keymap.set with placeholders for mode, left hand side, rigth hand side
+-- and description. The trailing placeholder is for ease of use.
+  s("map", fmt("vim.keymap.set('{}', '{}', {{ desc = '{}' }}){}", { i(1), i(2), i(3), i(0) })),
 
-  -- simple text snippet 
-  -- s("snipt", fmt([[ 
-  --     <>(<>, {t('<>')}<>
-  --     <>)<>,]],
-  --     { c(1, {t("s"), t("autosnippet")}), c(2, {i(nil, "trig"), sn(nil, {t("{trig='"), i(1), t("'}")})}), i(3, "text"), i(4, "opts"), i(5), i(0)},
-  --     { delimiters='<>' }
-  --   )),
 })
