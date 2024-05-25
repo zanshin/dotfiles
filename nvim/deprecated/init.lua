28ed7ddbf833ef0275c0f3ee113efcba8ef9c29d
Mark Nichols <mark@zanshin.net>
2024-05-24 21:36:06 -0500

    Deprecating old init file

---
+++ nvim/deprecated/init.lua
@@ -0,0 +1,7 @@
+-- require("zanshin.helpers")  -- functions to wrap commands
+-- require("zanshin.mappings") -- mappings
+-- require("zanshin.lazy")     -- manage plugins
+-- require("zanshin.options")  -- anything that gets set
+-- require("zanshin.colors")   -- color theme
+-- require("zanshin.autocmds") -- autocommands
+-- require("zanshin.diagnostics") -- LSP diagnostic settings
--- nvim/lua/zanshin/init.lua
+++
@@ -1,7 +0,0 @@
-require("zanshin.helpers")  -- functions to wrap commands
-require("zanshin.mappings") -- mappings
-require("zanshin.lazy")     -- manage plugins
-require("zanshin.options")  -- anything that gets set
-require("zanshin.colors")   -- color theme
-require("zanshin.autocmds") -- autocommands
--- require("zanshin.diagnostics") -- LSP diagnostic settings
