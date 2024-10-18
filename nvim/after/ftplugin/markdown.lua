5e8102089014e8ddf37b2a8c2dd9344190c529ec
Mark Nichols <mark@zanshin.net>
2024-10-17 22:47:40 -0500

    Markdown plugin added

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -9,3 +9,7 @@ vim.opt.linebreak = true   -- line break on while words
 -- Allow j/k when navigating wrapped lines
 vim.keymap.set('n', 'j', 'gj')
 vim.keymap.set('n', 'k', 'gk')
+
+-- Obsidian requires this
+-- 0 - default, 1 or 2 are optional values see help
+vim.opt.conceallevel = 1
