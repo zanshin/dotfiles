d2daaf72415210e5e0f1102f83ac525d89a9c8be
Mark Nichols <mark@zanshin.net>
2024-11-14 23:09:24 -0600

    Python formatting

--- mutt/offlineimap/offlineimap.py
+++ mutt/offlineimap/offlineimap.py
@@ -3,8 +3,8 @@
 import subprocess
 
 def mailpassword(account):
-  path = "/home/mark/.mutt/passwords/%s.gpg" % account
-  try:
-    return subprocess.check_output(["gpg", "--quiet", "--batch", "-d", path]).strip()
-  except subprocess.CalledProcessError:
-    return ""
+    path = "/home/mark/.mutt/passwords/%s.gpg" % account
+    try:
+        return subprocess.check_output(["gpg", "--quiet", "--batch", "-d", path]).strip()
+    except subprocess.CalledProcessError:
+        return ""
