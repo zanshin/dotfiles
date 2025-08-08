3b6941c287b120066fbb7c5be23323b1abbd50c5
Mark Nichols <mark@zanshin.net>
2025-08-08 09:48:42 -0500

    deprecated configurations moved to independent repository

--- deprecated/vim/snippets/chef/cookbook_file.rb
+++
@@ -1,7 +0,0 @@
-cookbook_file "__path_to_file__" do
-  owner "__owner__"
-  group "__group__"
-  mode "__0755__"
-  source "__source__"
-  action :create
-end
--- deprecated/vim/snippets/chef/package.rb
+++
@@ -1,4 +0,0 @@
-package "__name__" do
-  version "__version__"
-  action :install
-end
