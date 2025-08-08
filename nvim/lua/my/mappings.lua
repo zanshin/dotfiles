600fda8922ab13df6a339c80813d0f7d4cde7d4e
Mark Nichols <mark@zanshin.net>
2025-08-08 10:26:18 -0500

    git-blame toggle mapping added

--- nvim/lua/my/mappings.lua
+++ nvim/lua/my/mappings.lua
@@ -37,3 +37,7 @@ vim.keymap.set('n', '<leader><leader>x', '<cmd>source %<cr>', { desc = 'Execute 
 
 -- Sudo to write protected file
 vim.keymap.set('c', 'w!!', '!sudo tee % > /dev/null', { desc = 'Use sudo to write protected file' })
+
+-- Git Blame
+vim.keymap.set("n", "<leader>gb", ":GitBlameToggle<cr>", { desc = 'Toggle Git [B]lame on / off' })
+
