eaffed44a32ce26bfd3ec1fd155cc31ab0ac361a
Mark Nichols <mark@zanshin.net>
2025-10-17 21:10:17 -0500

    swapping back to ayu dark colors

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,23 +2,34 @@
 -- August 4.2025
 
 return {
-  'Shatur/neovim-ayu',
-  lazy = false,
-  priority = 1000,
-  opts = {
-    mirage = false,
-    overrides = {
-      Comment = { fg = "#b0c4de" },
-      LineNr = { fg = "#778899" },
-      Folded = { fg = "#6495ed" },
-      FoldColumn = { fg = "#6495ed" },
-      LspInlayHint = { fg = "#778899" },
-    },
-  },
-  config = function(_, opts)
-    require('ayu').setup(opts)
-    vim.opt.termguicolors = true -- enable 24-bit RGB colors
-    vim.cmd('set background=dark')
-    vim.cmd("colorscheme ayu-dark")
-  end
+	"Shatur/neovim-ayu",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		mirage = false,
+		overrides = {
+			Comment = { fg = "#b0c4de" },
+			LineNr = { fg = "#778899" },
+			Folded = { fg = "#6495ed" },
+			FoldColumn = { fg = "#6495ed" },
+			LspInlayHint = { fg = "#778899" },
+		},
+	},
+	config = function(_, opts)
+		require("ayu").setup(opts)
+		vim.opt.termguicolors = true -- enable 24-bit RGB colors
+		vim.cmd("set background=dark")
+		vim.cmd("colorscheme ayu-dark")
+	end,
 }
+
+-- return {
+-- 	{
+-- 		"zanshin/nvim-fourcolor-theme",
+-- 		lazy = false,
+-- 		priority = 1000,
+-- 		config = function()
+-- 			vim.cmd.colorscheme("fourcolor")
+-- 		end,
+-- 	},
+-- }
